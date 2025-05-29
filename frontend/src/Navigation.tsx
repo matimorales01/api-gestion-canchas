629b4c2ef45cca4429f4ddccdc3230ec3cceec0c
Matias <matmorales@fi.uba.ar>
2025-05-29 16:57:35 -0300

    Agregado el front del panel de administracion

--- frontend/src/Navigation.tsx
+++ frontend/src/Navigation.tsx
@@ -5,40 +5,48 @@ import { MainScreen } from "@/screens/MainScreen";
 import { SignupScreen } from "@/screens/SignupScreen";
 import { useToken } from "@/services/TokenContext";
 import { CanchaScreen } from "@/screens/CrearCancha";
+import AdminCanchasScreen from "@/screens/AdminCanchasScreen";
+import EditarCanchaScreen from "@/screens/EditarCanchaScreen"; // <--- IMPORT NUEVO
 
 export const Navigation = () => {
-  const [tokenState] = useToken();
-  switch (tokenState.state) {
-    case "LOGGED_IN":
-      return (
-        <Switch>
-          <Route path="/">
-            <MainScreen />
-          </Route>
-          <Route path="/crear-cancha">
-            <CanchaScreen />
-          </Route>
-          <Route>
-            <Redirect href="/" />
-          </Route>
-        </Switch>
-      );
-    case "LOGGED_OUT":
-      return (
-        <Switch>
-          <Route path="/login">
-            <LoginScreen />
-          </Route>
-          <Route path="/signup">
-            <SignupScreen />
-          </Route>
-          <Route>
-            <Redirect href="/signup" />
-          </Route>
-        </Switch>
-      );
-    default:
-      // Make the compiler check this is unreachable
-      return tokenState satisfies never;
-  }
+    const [tokenState] = useToken();
+    switch (tokenState.state) {
+        case "LOGGED_IN":
+            return (
+                <Switch>
+                    <Route path="/">
+                        <MainScreen />
+                    </Route>
+                    <Route path="/crear-cancha">
+                        <CanchaScreen />
+                    </Route>
+                    <Route path="/admin/canchas">
+                        <AdminCanchasScreen />
+                    </Route>
+                    <Route path="/admin/canchas/:id"> {/* <--- NUEVA RUTA */}
+                        <EditarCanchaScreen />
+                    </Route>
+                    <Route>
+                        <Redirect href="/" />
+                    </Route>
+                </Switch>
+            );
+        case "LOGGED_OUT":
+            return (
+                <Switch>
+                    <Route path="/login">
+                        <LoginScreen />
+                    </Route>
+                    <Route path="/signup">
+                        <SignupScreen />
+                    </Route>
+                    <Route>
+                        <Redirect href="/signup" />
+                    </Route>
+                </Switch>
+            );
+        default:
+            // Make the compiler check this is unreachable
+            return tokenState satisfies never;
+    }
 };
