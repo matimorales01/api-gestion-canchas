b0df3026e1afdc4e94ac6e298f22a935fc99eaa7
Imanol Suarez Pino <isuarezp@fi.uba.ar>
2025-06-08 17:13:53 -0300

    Edicion de torneo planteada inicialmene

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/torneo/TorneoRestController.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/torneo/TorneoRestController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import jakarta.validation.Valid;
-import ar.uba.fi.ingsoft1.todo_template.torneo.dto.*;
+import ar.uba.fi.ingsoft1.todo_template.torneo.dto.TorneoCreateDTO;
+import ar.uba.fi.ingsoft1.todo_template.torneo.dto.TorneoDTO;
+import ar.uba.fi.ingsoft1.todo_template.torneo.dto.TorneoUpdateDTO;
 
 @RestController
 @RequestMapping("/torneos")
@@ -18,32 +20,26 @@ public class TorneoRestController {
 
     @GetMapping
     public ResponseEntity<List<TorneoDTO>> all() {
-        var lista = service.listTorneos().stream()
-            .map(Torneo::toDTO)
-            .toList();
+        List<TorneoDTO> lista = service.listTorneos().stream().map(Torneo::toDTO).toList();
         return ResponseEntity.ok(lista);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TorneoDTO> one(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getTorneo(id).toDTO());
+    public ResponseEntity<TorneoDTO> one(@PathVariable Long id_de_torneo) {
+        return ResponseEntity.ok(service.getTorneo(id_de_torneo).toDTO());
     }
 
     @PostMapping
-    public ResponseEntity<TorneoDTO> create(
-        @Valid @RequestBody TorneoCreateDTO dto
-    ) {
-        Torneo creado = service.createTorneo(dto);
+    public ResponseEntity<TorneoDTO> create(@Valid @RequestBody TorneoCreateDTO dto_de_creacion) {
+        Torneo creado = service.createTorneo(dto_de_creacion);
         return ResponseEntity.status(HttpStatus.CREATED).body(creado.toDTO());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TorneoDTO> update(
-        @PathVariable Long id,
-        @Valid @RequestBody TorneoUpdateDTO dto
-    ) {
-        Torneo actualizado = service.updateTorneo(id, dto);
-        return ResponseEntity.ok(actualizado.toDTO());
+    public ResponseEntity<TorneoDTO> edit(@PathVariable Long id, @Valid @RequestBody TorneoUpdateDTO dto_de_actualizacion) {
+        Torneo actualizado = service.updateTorneo(id, dto_de_actualizacion);
+        //le pongo ese success message ya que es lo que se espera en el frontend de postman para ver el resultado bien
+        return ResponseEntity.ok().header("X-Success-Message", "Cambios guardados exitosamente").body(actualizado.toDTO());
     }
 
     @DeleteMapping("/{id}")
