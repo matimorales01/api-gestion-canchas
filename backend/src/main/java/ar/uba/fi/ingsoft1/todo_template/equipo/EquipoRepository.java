bcfb91b8727085f5efa2e7ba80fd74752457041a
Tomás Francisco Amundarain <tamundarain@fi.uba.ar>
2025-06-12 17:35:22 -0300

    Feature de get mis equipos

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/equipo/EquipoRepository.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/equipo/EquipoRepository.java
@@ -1,9 +1,13 @@
 package ar.uba.fi.ingsoft1.todo_template.equipo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EquipoRepository extends JpaRepository<Equipo, String>  {
     boolean existsByTeamName(String teamName);
+    Optional<List<Equipo>> findByCaptainId(Integer id);
 }
