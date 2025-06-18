fa0b923c023e3d8bea71e472b0f168951b7e0efc
Tomás Francisco Amundarain <tamundarain@fi.uba.ar>
2025-06-18 02:21:58 -0300

    Cambio del patron state por la clase StateReserva

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/reserva/StateReserva.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/reserva/StateReserva.java
@@ -2,11 +2,50 @@ package ar.uba.fi.ingsoft1.todo_template.reserva;
 
 import ar.uba.fi.ingsoft1.todo_template.partido.Partido;
 import ar.uba.fi.ingsoft1.todo_template.partido.TipoPartido;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 
+@Embeddable
+public class StateReserva {
+    @Column(nullable = false)
+    private String state = "DISPONIBLE";
 
-public interface StateReserva {
-    String getState();
-    TipoPartido getTipoPartido();
-    String getOrganizadorEmail();
-    StateReserva changeState(Partido partido);
+    @Column(nullable = true)
+    private TipoPartido tipoPartido = null;
+    
+    @Column(nullable = true)
+    private String emailOrganizador = null;
+    
+
+    public String getState() {
+        return state;
+    }   
+    
+
+    public TipoPartido getTipoPartido() {
+        return tipoPartido;
+    }
+
+    public String getOrganizadorEmail() {
+        return emailOrganizador;
+    }
+
+    public StateReserva changeState(Partido partido) {
+        if (partido == null) {
+            this.state = "DISPONIBLE";
+            this.tipoPartido = null;
+            this.emailOrganizador = null;
+            
+            return this;
+        }
+
+        TipoPartido tipo = tipoPartido != null ? tipoPartido : null;
+        String email = partido.getOrganizador().getEmail();
+
+        this.tipoPartido = tipo;
+        this.emailOrganizador = email;
+        this.state = "OCUPADA";
+
+        return this;
+    }
 }
